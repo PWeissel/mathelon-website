e59eafd8d46e464eed0c9b360206c642bdb4fe0c
Philipp <philipp@weissel.eu>
2025-08-04 10:27:23 +0200

    dynamic sources

---
+++ src/content/config.ts
@@ -0,0 +1,43 @@
+import { defineCollection, z } from 'astro:content';
+
+const homepage = defineCollection({
+  type: 'content',
+  schema: z.object({
+    hero: z.object({
+      headline: z.string(),
+      subline: z.string(),
+      primary_button_text: z.string(),
+      secondary_button_text: z.string(),
+      primary_button_link: z.string(),
+      secondary_button_link: z.string(),
+      philipp_image: z.string().optional(),
+      product_image: z.string().optional(),
+    }),
+    problem: z.object({
+      headline: z.string(),
+    }),
+    solution: z.object({
+      headline: z.string(),
+    }),
+    success: z.object({
+      headline: z.string(),
+    }),
+    applications: z.object({
+      headline: z.string(),
+    }),
+    authority: z.object({
+      headline: z.string(),
+      empathy_text: z.string(),
+    }),
+    main_cta: z.object({
+      headline: z.string(),
+      description: z.string(),
+      button_text: z.string(),
+      button_link: z.string(),
+    }),
+  }),
+});
+
+export const collections = {
+  homepage,
+};
